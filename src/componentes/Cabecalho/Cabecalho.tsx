4fb67fe9588e7bf54d89dcd1d407e9bd0967b390
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-07-25 11:29:53 -0300

    Cria arquivo e estilos para Cabecalho

---
+++ src/componentes/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,11 @@
+import styles from "./Cabecalho.module.css";
+
+export default function Cabecalho() {
+  return (
+        <header className={styles.cabecalho} >
+            <h1>
+                Exercicio React
+            </h1>
+        </header>
+  )
+}
