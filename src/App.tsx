f7dcaba9b8537a8b1202b5e039c622ba77bbf9ea
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-08-05 19:58:33 -0300

    Atualiza com rotas para paginas e importações

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import Cabecalho from "./componentes/Cabecalho/Cabecalho";
 import Container from "./componentes/Container/Container";
 import Menu from "./componentes/Menu/Menu";
-import Home from "./pages/Home";
-import Pagina404 from "./pages/Pagina404";
 import styles from "./App.module.css";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Servicos from "./pages/Servicos";
+import Contato from "./pages/Contato";
+import Pagina404 from "./pages/Pagina404";
 
 export default function App() {
   return (
@@ -18,6 +21,9 @@ export default function App() {
         <Container>
           <Routes >
             <Route path="/" element={<Home />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/servicos" element={<Servicos />} />
+            <Route path="/contato" element={<Contato />} />
             <Route path="*" element={<Pagina404 />} />
           </Routes>
         </Container>
