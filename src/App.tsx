f022ccc796a911cc574e1248329c4b6b10cbbf59
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-08-05 15:43:16 -0300

    Atualiza conteudo dentro da main container

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import Cabecalho from "./componentes/Cabecalho/Cabecalho";
+import Container from "./componentes/Container/Container";
 import Conteudo from "./componentes/Conteudo/Conteudo";
+import Menu from "./componentes/Menu/Menu";
 
 export default function App() {
   return (
     <>
       <Cabecalho />
-
-      <Conteudo />
+      <Container>
+        <Menu />
+        <Conteudo />
+      </Container>
     </>
   );
 }
