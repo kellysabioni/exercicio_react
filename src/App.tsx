d4c1644101c1e6989a1d447404c889a28c7b64d4
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-08-05 19:20:20 -0300

    Atualiza conteudo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
+
+import { Route, Routes } from "react-router-dom";
 import Cabecalho from "./componentes/Cabecalho/Cabecalho";
 import Container from "./componentes/Container/Container";
-import Conteudo from "./componentes/Conteudo/Conteudo";
 import Menu from "./componentes/Menu/Menu";
+import Home from "./pages/Home";
+import Pagina404 from "./pages/Pagina404";
+import styles from "./App.module.css";
 
 export default function App() {
   return (
     <>
       <Cabecalho />
-      <Container>
+      <section className={styles.geral} >
+
         <Menu />
-        <Conteudo />
-      </Container>
+
+        <Container>
+          <Routes >
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Pagina404 />} />
+          </Routes>
+        </Container>
+      
+      
+      </section>
     </>
   );
 }
