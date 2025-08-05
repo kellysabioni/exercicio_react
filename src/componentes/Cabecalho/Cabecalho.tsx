0cfba2755b47bb6bcc35e972b549372ad977e74a
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-08-05 18:17:07 -0300

    Atualiza pagina Cabecalho e seu estilo

--- src/componentes/Cabecalho/Cabecalho.tsx
+++ src/componentes/Cabecalho/Cabecalho.tsx
@@ -1,10 +1,15 @@
+// Ensure your project supports importing images and CSS modules
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
 import styles from "./Cabecalho.module.css";
 
 export default function Cabecalho() {
   return (
     <>
       <header className={styles.cabecalho}>
-        <h1>Maraja Eventos</h1>
+        <h1> <img src={logo} alt="Logo da empresa, Imagem de um elefante" />
+        <Link to="/">Maraja Eventos</Link>
+        </h1>
       </header>
     </>
   );
