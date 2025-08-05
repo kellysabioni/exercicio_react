094ef41800877faf3ad8fd79536426b0bbed1db1
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-08-05 18:44:57 -0300

    Atualiza pagina 404 e aplica estilos

--- src/pages/Pagina404.tsx
+++ src/pages/Pagina404.tsx
@@ -1,5 +1,18 @@
+import { useEffect } from 'react';
+import logo from '../assets/logo-branca.png'
+import { Link } from 'react-router-dom';
+import styles from "../componentes/Container/Container.module.css";
+
 export default function Pagina404() {
+  useEffect(() => {
+    document.title = "Pagina Não Encontrada | Maraja Eventos";
+  }, []);
   return (
-    <div>Pagina404</div>
+    <div className={styles.notFound} >
+      <h2><img src={logo} alt="Imagem de um elefante" /></h2>
+      <h3>Desculpe!</h3>
+      <p>Página Não encontrada</p>
+      <Link to="/">Voltar para a página inicial</Link>
+    </div>
   )
 }
