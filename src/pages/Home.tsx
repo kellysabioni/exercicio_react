bc774478c8fbd3c9807b186f181a328bc97b5294
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-08-05 19:58:13 -0300

    Atualiza Homoe

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    document.title = " Home | Maraja Eventos";
+  }, []);
   return (
-    <div>Home</div>
+    <div>
+
+    <p> <strong>Maraja Eventos</strong> oferece serviços com foco nos nichos de moda, corporativo e feiras. <br />
+    Para atender o seu projeto com êxito, é recomendado consultar a equipe para que eles possam entender melhor o seu evento.</p>
+    
+    <p>Disponibilizamos recursos como segurança, almoço, modelos, bombeiros, buffet, decoração, garçons, limpeza, logística, barman, estruturas e criativo.</p>
+    </div>
   )
 }
