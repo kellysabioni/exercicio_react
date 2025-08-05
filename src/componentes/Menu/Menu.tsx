381e9d63efaa29f2ae9d371e3ebaec62716c7699
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-08-05 15:42:23 -0300

    Organiza arquivos e atualiza estilos para Manu

---
+++ src/componentes/Menu/Menu.tsx
@@ -0,0 +1,14 @@
+import "./Menu.module.css";
+
+export default function Menu() {
+  return (
+    <>
+        <nav>
+            <a href=""> 1 </a>
+            <a href=""> 2 </a>
+            <a href=""> 3 </a>
+            <a href=""> 4 </a>
+        </nav>
+    </>
+  )
+}
